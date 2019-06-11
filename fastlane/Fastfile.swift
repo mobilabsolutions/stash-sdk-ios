1c21af02c63f6b3487a978297f63edccf30fb322
Robert Rabe <robert.rabe1@icloud.com>
2019-06-11 16:49:58 +0200

    PSOS-258 Initial fastlane configuration

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,52 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+    func testLane() {
+        desc("Runs Unit and UI tests")
+        self.unitTestLane()
+        self.uiTestLane()
+    }
+
+    func uiTestLane() {
+        desc("Run UI tests")
+        runTests(project: "MobilabPayment.xcodeproj", scheme: "Sample", device: "iPhone X", prelaunchSimulator: true)
+    }
+
+    func unitTestLane() {
+        desc("Run Unit tests")
+        runTests(project: "MobilabPayment.xcodeproj", scheme: "MobilabPaymentTests", prelaunchSimulator: true)
+    }
+
+    func betaLane() {
+        desc("Distribute to Beta")
+        let changeLog = changelogFromGitCommits(mergeCommitFiltering: "exclude_merges")
+        let buildSecret = environmentVariable(get: "CRASHLYTICS_BUILD_SECRET")
+        let apiKey = environmentVariable(get: "CRASHLYTICS_API_KEY")
+        betaSample(buildSecret: buildSecret, apiKey: apiKey, changeLog: changeLog)
+        betaDemo(buildSecret: buildSecret, apiKey: apiKey, changeLog: changeLog)
+    }
+
+    private func betaSample(buildSecret: String, apiKey: String, changeLog: String) {
+        incrementBuildNumber()
+        buildApp(project: "MobilabPayment.xcodeproj", scheme: "Sample", clean: false,
+                 includeBitcode: false, exportMethod: "ad-hoc")
+        crashlytics(crashlyticsPath: "./Sample/other/Crashlytics.framework/submit",
+                    apiToken: apiKey, buildSecret: buildSecret, notes: changeLog, groups: "payment-sdk-testers")
+    }
+
+    private func betaDemo(buildSecret: String, apiKey: String, changeLog: String) {
+        incrementBuildNumber()
+        buildApp(project: "MobilabPayment.xcodeproj", scheme: "Demo", clean: false,
+                 includeBitcode: false, exportMethod: "ad-hoc")
+        crashlytics(crashlyticsPath: "./Sample/other/Crashlytics.framework/submit",
+                    apiToken: apiKey, buildSecret: buildSecret, notes: changeLog, groups: "payment-sdk-testers")
+    }
+}
---
+++ fastlane/Gymfile.swift
@@ -0,0 +1,12 @@
+// For more information about this configuration visit
+// https://docs.fastlane.tools/actions/gym/#gymfile
+
+// In general, you can use the options available
+// fastlane gym --help
+
+// Remove the // in front of the line to enable the option
+
+class Gymfile: GymfileProtocol {
+    // var sdk: String { return "iphoneos12.0" }
+    // var outputDirectory: String { return "./" }
+}
