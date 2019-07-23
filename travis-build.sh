fc84c73b0e36595f2c1e1c704d5a341b57ee3b70
Robert Rabe <robert.rabe1@icloud.com>
2019-07-23 09:04:47 +0200

    NO_TICKET Move compilation of Objective-C Sample into one single build step

--- travis-build.sh
+++ travis-build.sh
@@ -7,13 +7,16 @@ echo "Starting Test run";
 if [ "$TEST_SUITE" = 'unit' ]; then
     echo "Running unit tests.";
     fastlane unitTest;
+
+    echo "Building Objective-C Sample";
+    xcodebuild -workspace "$WORKSPACE_NAME" -scheme "Sample-ObjC" -destination "$DESTINATION" | xcpretty -f `xcpretty-travis-formatter`;
 elif [ "$TEST_SUITE" = 'ui' ]; then
     echo "Running UI tests.";
     fastlane uiTest;
 else 
     echo "Running unit and UI tests.";
     fastlane test;
-fi
 
-echo "Building Objective-C Sample";
-xcodebuild -workspace "$WORKSPACE_NAME" -scheme "Sample-ObjC" -destination "$DESTINATION" | xcpretty -f `xcpretty-travis-formatter`;
+    echo "Building Objective-C Sample";
+    xcodebuild -workspace "$WORKSPACE_NAME" -scheme "Sample-ObjC" -destination "$DESTINATION" | xcpretty -f `xcpretty-travis-formatter`;
+fi
